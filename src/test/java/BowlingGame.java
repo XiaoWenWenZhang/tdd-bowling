d18039115e630b2d3ae3b320d936f9dfb37d6428
xiaowen <846703636@qq.com>
2020-09-14 20:11:53 +0800

    implement return number when tenth rond neither strike nor spare

--- src/test/java/BowlingGame.java
+++ src/test/java/BowlingGame.java
@@ -3,41 +3,31 @@ public class BowlingGame {
     public int calulate(String s) {
         if(s.equals("0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"))        return 0;
         String[] str= s.split(",");
-        for(int i=0;i<str.length;i++){
-    //        System.out.println(str[i]);
-        }
-        int[] ints = new int[str.length];
-   //     System.out.println(str.length);
+
+        int[] its = new int[str.length];
 
         for(int i=0;i<str.length;i++){
-            ints[i] = Integer.parseInt(str[i]);
-      //      System.out.println(ints[i]);
+            its[i] = Integer.parseInt(str[i]);
         }
 
         int sum = 0;
         int[] score =new int[11];
         int rond=0;
         for(int i=0;rond<10;){
-            if(ints[i]==10){
-                score[rond] = ints[i]+ints[i+1]+ints[i+2];
-                System.out.println(score[rond]);
-                rond++;
+            if(its[i]==10){
+                score[rond++] = its[i]+its[i+1]+its[i+2];
                 i+=1;
-            }else if(ints[i] + ints[i+1] == 10){
-                score[rond] = ints[i]+ints[i+1]+ints[i+2];
-                System.out.println(score[rond]);
-                rond++;
+            }else if(its[i] + its[i+1] == 10){
+                score[rond++] = its[i]+its[i+1]+its[i+2];
                 i+=2;
             }else {
-                score[rond] = ints[i]+ints[i+1];
-                System.out.println(score[rond]);
-                rond++;
+                score[rond++] = its[i]+its[i+1];
                 i+=2;
             }
         }
         for (int c:score){
             sum+=c;
         }
-return  sum;
+        return  sum;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -11,24 +11,31 @@ public class BowlingGameTest {
     }
 
     @Test
-    void should_return_300_when_calculate_given_all_knockdown(){
+    void should_return_300_when_strike(){
         BowlingGame bowlingGame =new BowlingGame();
         int result = bowlingGame.calulate("10,10,10,10,10,10,10,10,10,10,10,10,10");
         assertEquals(300,result);
     }
 
     @Test
-    void  should_return_number_when_calculate_given_tenth_rond_firth_knockdown(){
+    void  should_return_number_when_tenth_rond_strike(){
         BowlingGame bowlingGame =new BowlingGame();
         int result = bowlingGame.calulate("5,3,10,7,3,4,4,10,5,2,4,2,7,2,5,5,10,6,3");
         assertEquals(128,result);
     }
 
     @Test
-    void  should_return_number_when_calculate_given_tenth_rond_second_patch(){
+    void  should_return_number_when_tenth_rond_spare(){
         BowlingGame bowlingGame =new BowlingGame();
         int result = bowlingGame.calulate("5,3,10,7,3,4,4,10,5,2,4,2,7,2,5,5,7,3,8");
         assertEquals(124,result);
+    }
+
+    @Test
+    void  should_return_number_when_tenth_rond_neither_strike_nor_spare(){
+        BowlingGame bowlingGame =new BowlingGame();
+        int result = bowlingGame.calulate("5,3,10,7,3,4,4,10,5,2,4,2,7,2,5,5,4,4");
+        assertEquals(111,result);
     }
 
 
